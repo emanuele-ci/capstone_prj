a3344fbe78bf77c2d2f73f37a9a187fb45cb2b94
emanuele-ci <emanuele.ciccarese@gmail.com>
2025-05-07 17:13:33 +0200

    Update auth.py use env variables
    Update auth.py use env variables

--- auth.py
+++ auth.py
@@ -1,13 +1,18 @@
 import json
+import os
 from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'ucie.eu.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'test1'
+#AUTH0_DOMAIN = 'ucie.eu.auth0.com'
+#ALGORITHMS = ['RS256']
+#API_AUDIENCE = 'test1'
+
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN') 
+ALGORITHMS = os.environ.get['ALGORITHMS'] 
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 ## AuthError Exception
 '''
