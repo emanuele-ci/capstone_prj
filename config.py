2ad6bd753e30bdef62a450fc04db122acc58d7c0
emanuele-ci <emanuele.ciccarese@gmail.com>
2025-05-08 12:33:46 +0200

    Update config.py rollback
    Update config.py rollback

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ import os
 
 
 
-#SECRET_KEY = os.urandom(32)
+SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
-#basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 #DB_NAME = os.environ.get("DB_NAME", 'capstone') 
-#DB_NAME = 'capstone' 
+DB_NAME = 'capstone' 
 #DB_USER=os.environ.get("DB_USER", 'postgres')
-#DB_USER='postgres'
+DB_USER='postgres'
 #DB_PASSWORD = os.environ.get("DB_PASSWORD", 'postgres')
-#DB_PASSWORD = 'postgres'
+DB_PASSWORD = 'postgres'
 
 
 class Config:
@@ -24,9 +24,9 @@ class Config:
    # Grabs the folder where the script runs.
    basedir = os.path.abspath(os.path.dirname(__file__))
    
-   AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN') 
-   ALGORITHMS = os.environ.get('ALGORITHMS') 
-   API_AUDIENCE = os.environ.get('API_AUDIENCE')
+   #AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN') 
+   #ALGORITHMS = os.environ.get('ALGORITHMS') 
+   #API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
    ASSISTANT_JWT = os.environ.get('ASSISTANT_JWT')
    PRODUCER_JWT = os.environ.get('PRODUCER_JWT')
@@ -36,8 +36,8 @@ class Config:
    
    
    # DATABASE URL
-   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost:5432/capstone'
-   #SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,'localhost:5432', DB_NAME)
+   #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost:5432/capstone'
+   SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(DB_USER,DB_PASSWORD,'localhost:5432', DB_NAME)
 
    #DATABASE_URL = os.environ.get('DATABASE_URL')
    
